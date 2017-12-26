211555703b48b5f7f720a34cdae40cc91a33f25b
Иван Водолазский <Vodolazskiy.I.A@sberbank.ru>
2017-12-26 17:44:09 +0300

    разнесение по файлам

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"net/http"
 	"github.com/jmoiron/sqlx"
-	"database/sql"
 	_ "github.com/lib/pq"
 
-	"./model"
-	"log"
-	"flag"
+	"./db"
 )
 
 func main() {
@@ -22,52 +19,24 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	case "/hi":
 		w.Write([]byte("OK"))
 	case "/database":
-		db := workWithDB()
 
+		var database *sqlx.DB
 
-		//todo именно тут крч надо сделать нормальный селект
-		if rows, err := db.dbConn.Query("SELECT ALL"); err != nil {
-			w.Write([]byte(err.Error()))
-		} else {
-			var something string
-			rows.Scan(&something)
-			w.Write([]byte(something))
+		database, _ = db.InitDb()
+
+		if (database != nil) {
+			//todo именно тут крч надо сделать нормальный селект
+			if rows, err := database.Query("SELECT ALL"); err != nil {
+				w.Write([]byte(err.Error()))
+			} else {
+				var something string
+				rows.Scan(&something)
+				w.Write([]byte(something))
+			}
 		}
-
 
 	default:
 		w.Write([]byte("DEF"))
 	}
 
-}
-
-func workWithDB() (*pgDb) {
-	var someString string
-	flag.StringVar(&someString, "db-connect", "host=/Users/TanDS/Library/Application\\ Support/Postgres/var-10 dbname=temp sslmode=disable", "DB Connect String")
-	flag.Parse()
-
-	dbConn, err := sqlx.Connect("postgres", someString)
-
-	if err != nil {
-		log.Printf(err.Error());
-	}
-
-	p := &pgDb{dbConn: dbConn}
-	return p
-}
-
-func (p *pgDb) selectPeople() ([]*model.Boss, error) {
-	people := make([]*model.Boss, 0)
-	if err := p.sqlSelectPeople.Select(&people); err != nil {
-		return nil, err
-	}
-	return people, nil
-}
-
-type pgDb struct {
-	dbConn *sqlx.DB
-
-	sqlSelectPeople *sqlx.Stmt
-	sqlInsertPerson *sqlx.NamedStmt
-	sqlSelectPerson *sql.Stmt
 }
